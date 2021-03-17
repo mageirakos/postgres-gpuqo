537b09d46eca8d8b4b42da0a671c30c7c27fe978
Riccardo Mancini <r.mancini@santannapisa.it>
2021-03-17 11:57:21 +0100

    draw_graph: add articulation detection and block coloring

--- misc/analysis/draw_graph.py
+++ misc/analysis/draw_graph.py
@@ -3,12 +3,89 @@ from os import system
 from tempfile import mkstemp
 
 from collections import defaultdict
+from pprint import pprint
 
 import pydot
 
-graph = pydot.Dot('my_graph', graph_type='graph')
+COLORSCHEME='dark28'
+COLORSCHEME_CYCLE = 8
 
+nodes = defaultdict(lambda: {
+    'visited': False, 
+    'depth': 0,
+    'low': 0,
+    'parent': None,
+    'articulation': False, 
+    'blocks': []
+})
 edges = defaultdict(list)
+
+def get_articulation_points(i, d):
+    nodes[i]['visited'] = True
+    nodes[i]['depth'] = d
+    nodes[i]['low'] = d
+
+    child_count = 0
+    is_articulation = False
+
+    for ni in edges[i]:
+        if not nodes[ni]['visited']:
+            nodes[ni]['parent'] = i
+            get_articulation_points(ni, d+1)
+            child_count += 1
+            if nodes[ni]['low'] >= nodes[i]['depth']:
+                is_articulation = True
+            nodes[i]['low'] = min(nodes[i]['low'], nodes[ni]['low'])
+        elif ni != nodes[i]['parent']:
+            nodes[i]['low'] = min(nodes[i]['low'], nodes[ni]['depth'])
+    if ((nodes[i]['parent'] is not None and is_articulation) 
+        or (nodes[i]['parent'] is None and child_count > 1)
+    ):
+        print(f"{i} is an articulation")
+        nodes[i]['articulation'] = True
+    else:
+        print(f"{i} is not an articulation")
+
+def color_blocks():
+    block_id = 0
+    for node in nodes:
+        if nodes[node]['articulation']:
+            continue
+        if nodes[node]['blocks']:
+            continue
+
+        block_id += 1
+
+        stack = [node]
+        visited = {n:False for n in nodes}
+        while stack:
+            n = stack.pop() 
+
+            if visited[n]:
+                continue
+
+            visited[n] = True
+            nodes[n]['blocks'].append(block_id)
+
+            if not nodes[n]['articulation']:
+                stack += edges[n]
+
+    for node in nodes:
+        if not nodes[node]['articulation']:
+            continue
+        for n in edges[node]:
+            if nodes[n]['articulation']:
+                if not any(
+                    b in nodes[n]['blocks'] for b in nodes[node]['blocks']
+                ):
+                    block_id += 1
+                    nodes[node]['blocks'].append(block_id)
+                    nodes[n]['blocks'].append(block_id)
+
+def block2col(block_id):
+    return str((block_id-1) % COLORSCHEME_CYCLE + 1)
+
+graph = pydot.Dot('my_graph', graph_type='graph')
 
 record = False
 for line in stdin:
@@ -34,23 +111,61 @@ for line in stdin:
             other_node_id = int(edge_desc.split()[0])
             edges[node_id].append(other_node_id)
 
-for node in edges.keys():
-    graph.add_node(pydot.Node(str(node)))
+if edges:
+    get_articulation_points(1, 0)
+    color_blocks()
 
-for node, other_nodes in edges.items():
-    for other_node in other_nodes:
-        if other_node > node: # do not add edges twice
-            graph.add_edge(pydot.Edge(str(node), str(other_node)))
+    pprint(nodes)
 
-print(edges)
+    for node in edges.keys():
+        if len(nodes[node]["blocks"]) > 1:
+            style = 'wedged'
+            fillcolor = ('"' 
+                + ':'.join(block2col(i) for i in nodes[node]["blocks"])
+                + '"'
+            )
+        elif len(nodes[node]["blocks"]) == 1:
+            style = 'filled'
+            fillcolor = block2col(nodes[node]["blocks"][0])
+        else:
+            style = 'filled'
+            fillcolor = "white"
 
-if len(argv) > 1:
-    out_file = argv[1]
-else:
-    out_file, out_filename = mkstemp()
+        style_kwargs = {
+            'style': style, 
+            'colorscheme': COLORSCHEME,
+            'fillcolor': fillcolor,
+            'fontcolor': 'white'
+        }
+        print(node, style_kwargs)
+            
+        graph.add_node(pydot.Node(
+            str(node),
+            **style_kwargs
+        ))
 
-graph.write_png(out_file)
-print(f"Output written to {out_filename}")
+    for node, other_nodes in edges.items():
+        for other_node in other_nodes:
+            if other_node > node: # do not add edges twice
+                common_block = [
+                    b 
+                    for b in nodes[node]['blocks'] 
+                    if b in nodes[other_node]['blocks']
+                ][0]
+                graph.add_edge(pydot.Edge(
+                    str(node), 
+                    str(other_node),
+                    colorscheme=COLORSCHEME,
+                    color=block2col(common_block)
+                ))
 
-if len(argv) == 1:
-    system(f"xdg-open {out_filename}")
+    if len(argv) > 1:
+        out_file = argv[1]
+    else:
+        out_file, out_filename = mkstemp()
+
+    graph.write_png(out_file)
+    print(f"Output written to {out_filename}")
+
+    if len(argv) == 1:
+        system(f"xdg-open {out_filename}")
