69fc0ecb33a2af13df75334a047d3c94e51e9bab
Riccardo Mancini <r.mancini@santannapisa.it>
2021-06-04 19:51:42 +0200

    remove duplicate function in cost_cout

--- src/backend/optimizer/gpuqo/gpuqo_cost_cout.cuh
+++ src/backend/optimizer/gpuqo/gpuqo_cost_cout.cuh
@@ -61,48 +61,4 @@ cost_hashjoin(BitmapsetN inner_rel_id, JoinRelation<BitmapsetN> &inner_rel,
     };
 }
 
-template<typename BitmapsetN>
-__host__ __device__
-static float 
-calc_join_cost(BitmapsetN left_rel_id, JoinRelation<BitmapsetN> &left_rel,
-                BitmapsetN right_rel_id, JoinRelation<BitmapsetN> &right_rel,
-                float join_rel_rows, GpuqoPlannerInfo<BitmapsetN>* info)
-{
-    float hj_cost, nl_cost, inl_cost, sm_cost;
-    float min_cost;
-
-    // hash join
-    hj_cost = HASHJOIN_COEFF * join_rel_rows + left_rel.cost + right_rel.cost;
-    min_cost = hj_cost;
-
-    // nested loop join
-    nl_cost = left_rel.cost + left_rel.rows * right_rel.cost;
-    min_cost = min(min_cost, nl_cost);
-    
-    // indexed nested loop join
-    if (has_useful_index(left_rel_id, right_rel_id, info)){
-        inl_cost = left_rel.cost 
-            + INDEXSCAN_COEFF * left_rel.rows * max(
-                                            join_rel_rows/left_rel.rows, 
-                                            1.0);
-        min_cost = min(min_cost, inl_cost);
-    }
-
-    // explicit sort merge
-    sm_cost = left_rel.cost + right_rel.cost
-        + SORT_COEFF * left_rel.rows * logf(left_rel.rows)
-        + SORT_COEFF * right_rel.rows * logf(right_rel.rows);
-    min_cost = min(min_cost, sm_cost);
-
-#if defined(SIMULATE_COMPLEX_COST_FUNCTION) && COST_FUNCTION_OVERHEAD>0
-    //Additional overhead to simulate complex cost functions
-        volatile long counter = COST_FUNCTION_OVERHEAD;
-        while (counter > 0){
-            --counter;
-        }
-#endif
-
-    return min_cost;
-}
-
 #endif
