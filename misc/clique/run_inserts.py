4ca8afd04f3349887ffa48fd58638516923c8260
Vasileios Mageirakos <mageirak@diascld30.iccluster.epfl.ch>
2022-07-17 10:51:59 +0200

    edit script

--- misc/clique/run_inserts.py
+++ misc/clique/run_inserts.py
@@ -4,19 +4,9 @@ import subprocess
 
 
 if __name__ == "__main__":
-    # STAR 3
-    num_dimension_tables = 200 # star3 has 2k dimension tables
-
-    # # dimensions
+    num_dimension_tables = 200
     for i in tqdm(range(1,num_dimension_tables+1)):
         subprocess.run(
             f"psql -f inserts/insert_T_{i}.sql clique",
             shell=True,
         )
-    
-    
-    # print("Altering Tables statements...")
-    # subprocess.run(
-    #     f"psql -f add_foreign_keys.sql star3",
-    #     shell=True,
-    # )
